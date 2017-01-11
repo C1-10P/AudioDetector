74304a40100e13e151d15d9b53c37aa382a184c1
C1-10P <C1-10P@users.noreply.github.com>
2017-01-11 21:58:55 +0100

    install

--- install-raspberry.sh
+++ install-raspberry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+apt-get install -y python-pyaudio
 #pyaudio in default version is not working correctly
-#apt-get install -y python-pyaudio
-lib/pyaudio/install-raspberry-pyaudio.sh
+#lib/pyaudio/install-raspberry-pyaudio.sh
 apt-get install -y python-numpy
 apt-get install -y python-matplotlib
 
